c2b6073c22443fe46ce10dbbe12a639514e5ed7a
masripada <masripada@deloitte.com>
2026-06-16 17:13:27 +0530

    Remove MY_SECRET_KEY handling from HTTP trigger
    Removed the retrieval of MY_SECRET_KEY and its usage in the response body.

--- HttpsTrigger/index.js
+++ HttpsTrigger/index.js
@@ -1,15 +1,12 @@
 module.exports = async function (context, req) {
-  const mySecret = process.env.MY_SECRET_KEY;
-    if (!mySecret) {
-        throw new Error("Missing required env var: MY_SECRET_KEY");
-    }
+ 
     context.log('HTTP trigger function processed a request.');
     context.res = {
         status: 200,
         headers: { 'Content-Type': 'application/json' },
         body: {
             message: 'Hi from Episode changes 4.2 of the ProdXcel Workshop!',
-            secret: mySecret
+            
         }
     };
 };
