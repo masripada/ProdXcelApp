86f38ac0a3bcd837a523dce40aa1590096f72878
masripada <masripada@deloitte.com>
2026-06-16 17:10:28 +0530

    Remove MY_SECRET_KEY validation
    Removed check for missing MY_SECRET_KEY environment variable.

--- HttpsTrigger/index.js
+++ HttpsTrigger/index.js
@@ -1,11 +1,5 @@
 module.exports = async function (context, req) {
  
-   
-    const mySecret = process.env.MY_SECRET_KEY;
-    if (!mySecret) {
-        throw new Error("Missing required env var: MY_SECRET_KEY");
-    }
- 
     context.log('HTTP trigger function processed a request.');
     context.res = {
         status: 200,
